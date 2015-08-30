b36008484196240639e4f9ffb6a4895c063b840b
Douglas Goddard <douglas.gastonguay@gmail.com>
2015-08-29 22:02:18 -0400

    Adding favicon.

--- html/lifegraph.php
+++ html/lifegraph.php
@@ -17,6 +17,25 @@ sec_session_start();
             mixpanel.init("3cfcfcdc33cbc6693645f79e38e9a292");
         </script>
         <!-- end Mixpanel -->
+
+        <link rel="apple-touch-icon" sizes="57x57" href="icon/favicon/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="icon/favicon/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="icon/favicon/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="icon/favicon/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="icon/favicon/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="icon/favicon/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="icon/favicon/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="icon/favicon/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="icon/favicon/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" href="icon/favicon/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="icon/favicon/android-chrome-192x192.png" sizes="192x192">
+        <link rel="icon" type="image/png" href="icon/favicon/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="icon/favicon/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="icon/favicon/manifest.json">
+        <link rel="shortcut icon" href="icon/favicon/favicon.ico">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-TileImage" content="icon/favicon/mstile-144x144.png">
+        <meta name="msapplication-config" content="icon/favicon/browserconfig.xml">
         
         <!--script type="text/javascript" src="js/lifegraph.js"></script-->
         <!--link rel="stylesheet" href="styles/d3.css" /-->
