c85acddc5ff385cc0101ab3eef55d56fe8e3f554
Douglas Goddard <douglas.gastonguay@gmail.com>
2015-09-02 23:38:41 -0400

    Better placeholder names.

--- html/lifegraph.php
+++ html/lifegraph.php
@@ -144,16 +144,22 @@ sec_session_start();
             <div id="add_modal" class="modal modal-fixed-footer">
                 <div class="modal-content">
                     <div class="row">
-                    <div class="col s12">
-                        <ul class="tabs">
-                            <li class="tab col s3"><a href="#test1">Test 1</a></li>
-                            <li class="tab col s3"><a class="active" href="#test2">Test 2</a></li>
-                            <li class="tab col s3"><a href="#test4">Test 4</a></li>
-                        </ul>
-                    </div>
-                    <div id="test1" class="col s12">Test 1</div>
-                    <div id="test2" class="col s12">Test 2</div>
-                    <div id="test4" class="col s12">Test 4</div>
+                        <div class="col s12">
+                            <ul class="tabs">
+                                <li class="tab col s3"><a class="active" href="#create_person_div">Person</a></li>
+                                <li class="tab col s3"><a href="#create_event_div">Event</a></li>
+                                <li class="tab col s3"><a href="#create_location_div">Location</a></li>
+                            </ul>
+                        </div>
+                        <div id="create_person_div" class="col s12">
+                            PERSON
+                        </div>
+                        <div id="create_event_div" class="col s12">
+                            EVENT
+                        </div>
+                        <div id="create_location_div" class="col s12">
+                            LOCATION
+                        </div>
                     </div>
                 </div>
                 <div class="modal-footer">
