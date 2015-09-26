a87c4db2603db3c4e0842e5d96ed5f8daf829d6b
Douglas Goddard <douglas.gastonguay@gmail.com>
2015-09-25 23:00:46 -0400

    Added tick.

--- html/js/graph.js
+++ html/js/graph.js
@@ -17,7 +17,7 @@ Socialite.Graph.Connect['init'] = function() {
         .nodes([]) // initialize with a single node
         .linkDistance(150)
         .charge(-1000)
-        .on("tick", tick);
+        .on("tick", SGC.tick);
 
     var svg = d3.select("#graphDiv").append("svg")
         .attr("width", width)
@@ -33,3 +33,19 @@ Socialite.Graph.Connect['init'] = function() {
     SGC['link'] = svg.selectAll(".link");
     SGC['label'] = svg.selectAll("text.label"); 
 }
+
+Socialite.Graph.Connect['tick'] = function(e) {
+    link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+      //.attr("r", function(d) { return 25;});
+
+    label.attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+    });
+}
+
