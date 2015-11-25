c1b7f9d7fa98f5b1b4047a226c3f1381fb584991
Douglas Goddard <douglas.gastonguay@gmail.com>
2015-11-24 21:41:17 -0500

    Adding next step on click

--- html/js/tour.js
+++ html/js/tour.js
@@ -6,7 +6,34 @@ Socialite.Tour = {
             title: "Welcome to Socialite",
             content: "This guide will walk you through application functionality.",
             target: document.querySelector(".logo"),
-            placement: "right"
+            placement: "right",
+            onNext: function() {
+                $("#add_button").click(function() {
+                    $('#add_modal').openModal();
+                    $('ul.tabs').tabs();
+                    if($("#create_location_tab").hasClass('active'))
+                        Socialite.UI.refreshCreateMap();
+
+                    $("#create_submit_button").click(function() {
+                        var visibleType = undefined;
+                        if($("#create_person_tab").hasClass("active")) {
+                            visibleType = "person";
+                        } else if($("#create_event_tab").hasClass("active")) {
+                            visibleType = "event";
+                        } else if($("#create_location_tab").hasClass("active")) {
+                            visibleType = "location";
+                        } 
+
+                        if(visibleType == undefined) {
+                            return;
+                        }
+
+                        var formId = "#create_" + visibleType + "_form";
+                        $(formId).submit();
+                    });
+                    hopscotch.nextStep();
+                });
+            }
         },
         {
             title: "Add Node",
