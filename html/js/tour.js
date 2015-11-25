5de17a7f7fb82e8fba4666670e32f71e5f45797e
Douglas Goddard <douglas.gastonguay@gmail.com>
2015-11-24 21:40:08 -0500

    Updating tour

--- html/js/tour.js
+++ html/js/tour.js
@@ -3,10 +3,42 @@ Socialite.Tour = {
     id: "tour",
     steps: [
         {
+            title: "Welcome to Socialite",
+            content: "This guide will walk you through application functionality.",
+            target: document.querySelector(".logo"),
+            placement: "right"
+        },
+        {
             title: "Add Node",
             content: "Let's add a new node.",
             target: document.querySelector("#add_button"),
-            placement: "right"
+            placement: "right",
+            onNext: function() {
+                $("#add_button").click(function() {
+                    $('#add_modal').openModal();
+                    $('ul.tabs').tabs();
+                    if($("#create_location_tab").hasClass('active'))
+                        Socialite.UI.refreshCreateMap();
+
+                    $("#create_submit_button").click(function() {
+                        var visibleType = undefined;
+                        if($("#create_person_tab").hasClass("active")) {
+                            visibleType = "person";
+                        } else if($("#create_event_tab").hasClass("active")) {
+                            visibleType = "event";
+                        } else if($("#create_location_tab").hasClass("active")) {
+                            visibleType = "location";
+                        } 
+
+                        if(visibleType == undefined) {
+                            return;
+                        }
+
+                        var formId = "#create_" + visibleType + "_form";
+                        $(formId).submit();
+                    });
+                });
+            }
         },
         // {
         //     title: "My content",
