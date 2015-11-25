7028004f668c66a6ab186a1995f8e532a1337f87
Douglas Goddard <douglas.gastonguay@gmail.com>
2015-11-24 21:12:55 -0500

    Adding tour

---
+++ html/js/tour.js
@@ -0,0 +1,18 @@
+var Socialite = Socialite || {};
+Socialite.Tour = {
+    id: "hello-hopscotch",
+    steps: [
+        {
+            title: "Add Node",
+            content: "Let's add a new node.",
+            target: document.querySelector("#add_button"),
+            placement: "right"
+        },
+        // {
+        //     title: "My content",
+        //     content: "Here is where I put my content.",
+        //     target: document.querySelector("#content p"),
+        //     placement: "bottom"
+        // }
+    ]
+};
