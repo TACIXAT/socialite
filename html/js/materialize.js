63abd57c1e0740cafed1ad4253296fff57ec10ab
Douglas Goddard <douglas.gastonguay@gmail.com>
2016-01-02 12:14:31 -0500

    Remove vertical offset

--- html/js/materialize.js
+++ html/js/materialize.js
@@ -571,7 +571,7 @@ if ($) {
       // Position dropdown
       activates.css({
         position: 'absolute',
-        top: origin.position().top + verticalOffset,
+        top: origin.position().top,// + verticalOffset,
         left: leftPosition
       });
 
