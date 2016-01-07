ea09e20ccdf5a5fe6c79189df65b49f69a02ec3a
Douglas Goddard <douglas.gastonguay@gmail.com>
2016-01-07 18:18:05 -0500

    Add mixpanel to dev.php

--- html/dev.php
+++ html/dev.php
@@ -26,6 +26,24 @@
         <meta name="msapplication-TileImage" content="/icon/favicon/mstile-144x144.png">
         <meta name="msapplication-config" content="/icon/favicon/browserconfig.xml">
         <meta name="theme-color" content="#ffffff">
+        <?php
+            if(!isset($_SERVER['HTTP_DNT']) || $_SERVER['HTTP_DNT'] != 1) {
+                echo "<!-- start Mixpanel -->\n";
+                echo "        <script type=\"text/javascript\">\n";
+                echo "            (function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(\".\");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;\"undefined\"!==typeof d?c=b[d]=[]:d=\"mixpanel\";c.people=c.people||[];c.toString=function(b){var a=\"mixpanel\";\"mixpanel\"!==d&&(a+=\".\"+d);b||(a+=\" (stub)\");return a};c.people.toString=function(){return c.toString(1)+\".people (stub)\"};i=\"disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user\".split(\" \");\n";
+                echo "            for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement(\"script\");a.type=\"text/javascript\";a.async=!0;a.src=\"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js\";e=f.getElementsByTagName(\"script\")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);\n";
+                echo "            mixpanel.init(\"3cfcfcdc33cbc6693645f79e38e9a292\");\n";
+                echo "        </script>\n";
+                echo "        <!-- end Mixpanel -->\n";
+                echo "        <script type=\"text/javascript\">\n";
+                echo "            useMixpanel = true;\n";
+                echo "            mixpanel.track('visit');\n";
+            } else {
+                echo "<script type=\"text/javascript\">\n";
+                echo "            useMixpanel = false;\n";
+            }
+                echo "        </script>\n";
+        ?>
         <!--link href='https://fonts.googleapis.com/css?family=Playfair+Display+SC' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100' rel='stylesheet' type='text/css'-->
